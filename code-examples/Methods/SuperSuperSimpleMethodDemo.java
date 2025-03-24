d2a714ad0418426339b7c3d5a13eb70efe1b8ea9
rkjc <richardkjcross@gmail.com>
2025-03-24 15:09:29 -0700

    needed an even simpler example

---
+++ code-examples/Methods/SuperSuperSimpleMethodDemo.java
@@ -0,0 +1,21 @@
+package Methods;
+
+public class SuperSuperSimpleMethodDemo {
+    public static void main(String[] args) {
+        System.out.println("------ program is running -------");
+
+        int numA = 99;
+        int numB = 24;
+
+        int sum = add(numA, numB);
+
+        System.out.println(sum);
+
+        System.out.println("-------- program ended ---------");
+    } 
+
+    public static int add(int firstNum, int secondNum){
+        int tempSum = firstNum + secondNum;
+        return tempSum;
+    }
+}
